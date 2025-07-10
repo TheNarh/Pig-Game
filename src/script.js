87e9fd70a514227e5fd14869feaa84c15db5771c
Ludwig <41292435+TheNarh@users.noreply.github.com>
2025-07-10 16:46:58 +0000

    Added some animation and made the game feature complete.

--- src/script.js
+++ src/script.js
@@ -8,22 +8,34 @@ const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
 const current1El = document.getElementById('current--1');
 
-
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
+let scores, currentScore, activePlayer, playing;
+
 // Starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+const init = function() {
+    scores = [0, 0];
+    currentScore = 0;
+    activePlayer = 0;
+    playing = true;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
+    score0El.textContent = 0;
+    score1El.textContent = 0;
+    current0El.textContent = 0;
+    current1El.textContent = 0;
 
-const switchPlayer = function() {
+    diceEl.classList.add('hidden');
+    player0El.classList.remove('player--winner');
+    player1El.classList.remove('player--winner');
+    player0El.classList.add('player--active');
+    player1El.classList.remove('player--active');    
+}
+init();
+
+const switchPlayer = function () {
     document.getElementById(`current--${activePlayer}`).textContent = 0;
     currentScore = 0;
     activePlayer = activePlayer === 0 ? 1 : 0;
@@ -32,36 +44,53 @@ const switchPlayer = function() {
 }
 
 // Rolling dice functionality
-btnRoll.addEventListener('click', function() {
-    // 1. Generating a random dice roll
-    const dice = Math.trunc(Math.random() * 6) + 1;
+btnRoll.addEventListener('click', function () {
+    if (playing) {
 
-    // 2. Display dice
-    diceEl.classList.remove('hidden');
-    diceEl.src = `dice-${dice}.png`;
+        // 1. Generating a random dice roll
+        const dice = Math.trunc(Math.random() * 6) + 1;
 
-    // 3. Check if rolled 1
-    if (dice !== 1) {
-        // Add dice rolled to current score
-        currentScore += dice;
-        document.getElementById(`current--${activePlayer}`).textContent = currentScore;
-    } else {
-        // switch to next player
-        switchPlayer();
+        // 2. Display dice
+        diceEl.classList.remove('hidden');
+        diceEl.classList.add('animate');
+        diceEl.src = `dice-${dice}.png`;
+
+        // Remove animation after it ends
+        setTimeout(() => diceEl.classList.remove('animate'), 400);
+
+        // 3. Check if rolled 1
+        if (dice !== 1) {
+            // Add dice rolled to current score
+            currentScore += dice;
+            document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+        } else {
+            // switch to next player
+            switchPlayer();
+        }
     }
 })
 
-btnHold.addEventListener('click', function() {
-    // 1. Add current score to active player's score
-    scores[activePlayer] += currentScore;
+btnHold.addEventListener('click', function () {
+    if (playing) {
+        // 1. Add current score to active player's score
+        scores[activePlayer] += currentScore;
 
-    // scores[1] = scores[1] + currentScore; 
-    document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
+        // scores[1] = scores[1] + currentScore; 
+        document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
 
-    // 2. Check if player's score >= 100
-
-    // Finish the game
-
-    // Switch to the next player
-    switchPlayer();
+        // 2. Check if player's score >= 100
+        if (scores[activePlayer] >= 100) {
+            // Finish the game
+            playing = false;
+            diceEl.classList.add('hidden');
+            document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
+            document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');
+        } else {
+            // Switch to the next player
+            switchPlayer();
+        }
+    }
 })
+
+// Reset the game
+btnNew.addEventListener('click', init);
